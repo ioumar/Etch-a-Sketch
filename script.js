0087447478be40dc8c3a63d60bb382dbb15238ac
ioumar <108413180+ioumar@users.noreply.github.com>
2025-11-12 14:33:24 +0300

    Création d'une grille contenant 16 carrés en colonne et 16 en ligne

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const squareContainer = document.querySelector('.container');
+const squareNumber = 16;
 
-for(let i=0; i<16; i++){
-    let square = document.createElement('div');
-    square.classList.add('square');
-    squareContainer.appendChild(square);
+
+
+for(let i=0; i<squareNumber; i++){
+    for(let j=0; j<squareNumber; j++){
+        let square = document.createElement('div');
+        square.classList.add('square');
+        squareContainer.appendChild(square);
+    }
+
 }
