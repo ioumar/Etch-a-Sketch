6c5ee2abdcd86e34b6105f66b0e2832fac4632b9
ioumar <108413180+ioumar@users.noreply.github.com>
2025-11-12 14:08:45 +0300

    Création de 16 carrés alignés sur une ligne

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const squareContainer = document.querySelector('.container');
 
-let square = document.createElement('div');
-square.classList.add('square');
-squareContainer.appendChild(square);
+for(let i=0; i<16; i++){
+    let square = document.createElement('div');
+    square.classList.add('square');
+    squareContainer.appendChild(square);
+}
