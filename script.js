5cc3cede05e81a4f9623a9c2fcf6451fa97decd4
ioumar <108413180+ioumar@users.noreply.github.com>
2025-11-13 16:57:25 +0300

    Ajout d'un écouteur d'événement pour changer la couleur des carrés au survol

--- script.js
+++ script.js
@@ -5,16 +5,25 @@ const squareContainer = document.querySelector('.container');
 squareContainer.style.width = `${GRID_WIDTH}px`;
 squareContainer.style.height = `${GRID_WIDTH}px`;
 
+
 function createGrid(squareNumber){
+
     let squares = Math.pow(squareNumber,2);
+
     for(let i=0; i<squares; i++){
         let square = document.createElement('div')
         square.classList.add('square');
         square.style.width = `${GRID_WIDTH/NUMBER_OF_SQUARE}px`;
         square.style.height = `${GRID_WIDTH/NUMBER_OF_SQUARE}px`;
         squareContainer.appendChild(square);
-    }
         
+        
+        square.addEventListener('mouseover', () =>{
+            square.style.backgroundColor = "black";
+        });
+    }
+    
+    
 };
 
 createGrid(NUMBER_OF_SQUARE);
